f1ad284eb562092a0ac811f58a7137fff13b7999
Shaurya Singh <gaurav1152003@gmail.com>
2022-05-22 13:23:45 +0530

    Update ping.py

--- YukkiMusic/plugins/tools/ping.py
+++ YukkiMusic/plugins/tools/ping.py
@@ -1,3 +1,12 @@
+#
+# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+
 from datetime import datetime
 
 from pyrogram import filters
@@ -6,7 +15,6 @@ from pyrogram.types import Message
 from config import BANNED_USERS, MUSIC_BOT_NAME, PING_IMG_URL
 from strings import get_command
 from YukkiMusic import app
-from YukkiMusic.core.call import Yukki
 from YukkiMusic.utils import bot_sys_stats
 from YukkiMusic.utils.decorators.language import language
 
@@ -22,16 +30,14 @@ PING_COMMAND = get_command("PING_COMMAND")
 )
 @language
 async def ping_com(client, message: Message, _):
+    start = datetime.now()
     response = await message.reply_photo(
         photo=PING_IMG_URL,
         caption=_["ping_1"],
     )
-    start = datetime.now()
-    pytgping = await Yukki.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
-    resp = (datetime.now() - start).microseconds / 1000
+    end = datetime.now()
+    resp = (end - start).microseconds / 1000
     await response.edit_text(
-        _["ping_2"].format(
-            resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK,
-        )
+        _["ping_2"].format(resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK)
     )
